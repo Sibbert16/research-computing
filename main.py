0740c65c86bf5d9b8ed416449d1526ff157f10ae
Sam Hibbert <sibbert16@outlook.com>
2026-08-27 16:51:19 +0100

    Adding pytest to requirements

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
+from src.matrices import eigenvalues
 from src.vectors import magnitude, normalise
 
 v = np.array([3, 4])
+m = np.array([[1, 2], [3, 4]])
 
 print(magnitude(v))
 print(normalise(v))
+
+print(eigenvalues(m))
